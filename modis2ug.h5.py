07a46b4c00502a8a07ff7e8a1bb708ff5c27f275
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2019-04-03 14:01:56 -0500

    UIACCESS-66:updated plot code to include file name.

--- modis2ug.h5.py
+++ modis2ug.h5.py
@@ -52,5 +52,6 @@ m.drawmeridians(np.arange(-180, 180., 45.), labels=[0, 0, 0, 1])
 m.scatter(modis_lon, modis_lat, c=datam, s=1,
           edgecolors=None, linewidth=0)
 fig = plt.gcf()
+fig.suptitle('{0}'.format(file_name))
 pngfile = file_name+'.py.png'
 fig.savefig(pngfile)
