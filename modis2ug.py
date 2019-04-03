e3a4f2445e928d7fe541806057f8c40e61123ba4
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2019-04-03 11:34:07 -0500

    UIACCESS-66:fixed parameters and added retrieval function.

--- modis2ug.py
+++ modis2ug.py
@@ -23,8 +23,10 @@ file_name = '/Users/hyoklee/Downloads/misr_on_modis_SrcLowAnAfBlueGreen_Trg1KM8_
 # If USER_RESOLUTION is 0.05 or 0.01, nnInterpolate can be used.
 
 # Borrowed from http://hdfeos.org/zoo comprehensive example.
-x0, xinc, y0, yinc = (-180, 0.05, 90, -0.05)
+cellSize = 0.05
+x0, xinc, y0, yinc = (-180, cellSize, 90, -cellSize)
 nx, ny = (360*20, 180*20)
+# nx, ny = (360, 180)
 x = np.linspace(x0, x0 + xinc*nx, nx)
 y = np.linspace(y0, y0 + yinc*ny, ny)
 lon, lat = np.meshgrid(x, y)
@@ -43,23 +45,38 @@ with h5py.File(file_name, 'r') as f:
     modis_ds_lon = f['/Geolocation/Longitude']
     modis_lon = modis_ds_lon[:,:].astype(np.float64)
 
-# Set cell size and max radius.
-cellSize = 3
+# Set max radius.
 M_PI=3.14159265358979323846
 earthRadius = 6367444
 max_r = earthRadius * cellSize * M_PI / 180
 
-c = np.arange(cellSize, dtype=np.int32)
-i = np.arange(nx*ny, dtype=np.float64).reshape((nx,ny))
+index = np.arange(nx*ny, dtype=np.int32)
+distance = np.arange(nx*ny, dtype=np.float64).reshape((nx,ny))
 
 # Kent: try nnInterploate first.
 # In the summaryInterpolate, tarSD and nSouPixels are also output parameters.
-# This dumps core.
-pytaf.find_nn(modis_lat, modis_lon, cellSize, lat, lon, c, i, cellSize, max_r)
+n_src = modis_lat.size;
+print(n_src)
+n_trg = nx * ny;
+print(n_trg)
+
+# Find indexes of nearest neighbor point.
+pytaf.find_nn_block_index(modis_lat, modis_lon,
+                          n_src,
+                          lat, lon,
+                          index, distance,
+                          n_trg,
+                          max_r)
+print('Finished generating index.')
+# Get values for target.
+trg_data = np.arange(nx*ny, dtype=np.float64).reshape((nx,ny))
+pytaf.interpolate_nn(modis_data, trg_data, index, n_trg)
+print(trg_data)
+print('Finished retrieving data with index.')
 
 # Open file for writing.
 f2 = h5py.File('modis_on_ug.h5', 'w')
-dset = f2.create_dataset('/Target/Data_Fields/MISR_Radiance', data=i)
+dset = f2.create_dataset('/Target/Data_Fields/UG_Radiance', data=trg_data)
 dset2 = f2.create_dataset('/Source/Data_Fields/MODIS_Radiance', data=modis_data)
 
 # TODO: Add CF attributes on dataset.
