edfe74eb7950d1c825cbbfff83553a8ac3492f2d
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2019-04-18 13:41:54 -0500

    UIACCESS-77:added package name and description.

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ from Cython.Distutils import build_ext
 
 import numpy
 
-setup(
-  cmdclass = {'build_ext':build_ext},
-  ext_modules = cythonize([Extension("pytaf",
-                                     sources=["pytaf.pyx", "reproject.c"],
-                                     include_dirs=[numpy.get_include()])],
-                          gdb_debug=True)    
+setup(name='pytaf',
+      version='0.0.1',
+      description='Python wrapper for advanced funsion reprojection',
+      author='TerraFusion Team',
+      author_email='eoshelp@hdfgroup.org',
+      cmdclass = {'build_ext':build_ext},
+      ext_modules = cythonize([Extension("pytaf",
+                                         sources=["pytaf.pyx", "reproject.c"],
+                                         include_dirs=[numpy.get_include()])],
+                              gdb_debug=True)    
 )
