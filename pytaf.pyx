6b668731ecdd48f21704a2a37980543869ba5664
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2019-04-19 16:02:22 -0500

    UIACCESS-82:cleaned up main code.

--- pytaf.pyx
+++ pytaf.pyx
@@ -1,24 +1,25 @@
 """
 pytaf.pyx
 
-simple cython test of accessing advancedFusion functions
+A simple Cython-based Python wrapper for accessing advancedFusion functions
 
 The C functions are from advancedFusion:
 
  * reproject.c/clipping 
  * reproject.c/nnInterpolate
- * ... etc.
+ * ...
+ * etc.
 
 Tested under: Python 3.6.6 :: Anaconda custom (64-bit)
-Last updated: 2019-04-16
+Last updated: 2019-04-19
 """
 import numpy as np
 cimport cython
 
-# import both numpy and the Cython declarations for numpy
+# Import both numpy and the Cython declarations for numpy.
 cimport numpy as np
 
-# declare the interface to the C code
+# Declare the interface to the C code.
 cdef extern void clipping(double * val, double * mask, int nPixels)
 cdef extern void nearestNeighbor(double ** psouLat, double ** psouLon, int nSou,
                                  double * tarLat, double * tarLon,
@@ -33,57 +34,51 @@ cdef extern void nnInterpolate(double * souVal, double * tarVal,
 cdef extern void summaryInterpolate(double * souVal, int * souNNTarID, int nSou,
                                     double * tarVal, double * tarSD,
                                     int * nSouPixels, int nTar)
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def clip(np.ndarray[double, ndim=2, mode="c"] val not None,
          np.ndarray[double, ndim=2, mode="c"] mask not None,
          int nPixels):
-    """
-    clip(val, mask,  value)
-
-    param: val -- a 2-d numpy array of np.float64
-    param: mask -- a 2-d numpy array of np.float64
-    param: nPixesl -- an integer
-
-    """
-    clipping(&val[0,0], &mask[0,0], nPixels)
+    clipping(& val[0, 0], & mask[0, 0], nPixels)
     return None
+
 
 def find_nn(np.ndarray[double, ndim=2, mode="c"] psouLat not None,
             np.ndarray[double, ndim=2, mode="c"] psouLon not None,
             int nSou,
             np.ndarray[double, ndim=2, mode="c"] tarLat not None,
             np.ndarray[double, ndim=2, mode="c"] tarLon not None,
-            np.ndarray[int, ndim=1, mode="c"] tarNNSouID not None,        
+            np.ndarray[int, ndim=1, mode="c"] tarNNSouID not None,
             np.ndarray[double, ndim=2, mode="c"] tarNNDis not None,
             int nTar, double maxR):
-    
-    # See [1] for handling double pointers. 
-    cdef double* pLat = &psouLat[0,0]
-    cdef double* pLon = &psouLon[0,0]
 
-    nearestNeighbor(&pLat, &pLon, nSou,
-                    &tarLat[0,0], &tarLon[0,0], &tarNNSouID[0],
-                    &tarNNDis[0,0], nTar, maxR)
-    
+    cdef double * pLat = &psouLat[0, 0]
+    cdef double * pLon = &psouLon[0, 0]
+
+    nearestNeighbor(& pLat, & pLon, nSou,
+                     & tarLat[0, 0], & tarLon[0, 0], & tarNNSouID[0],
+                     & tarNNDis[0, 0], nTar, maxR)
+
+
 @cython.boundscheck(False)
-@cython.wraparound(False)        
+@cython.wraparound(False)
 def find_nn_block_index(
         np.ndarray[double, ndim=2, mode="c"] psouLat not None,
         np.ndarray[double, ndim=2, mode="c"] psouLon not None,
         int nSou,
         np.ndarray[double, ndim=2, mode="c"] tarLat not None,
         np.ndarray[double, ndim=2, mode="c"] tarLon not None,
-        np.ndarray[int, ndim=1, mode="c"] tarNNSouID not None,        
+        np.ndarray[int, ndim=1, mode="c"] tarNNSouID not None,
         np.ndarray[double, ndim=2, mode="c"] tarNNDis not None,
         int nTar, double maxR):
-    
-    # See [1] for handling double pointers.
-    cdef double* pLat = &psouLat[0,0]
-    cdef double* pLon = &psouLon[0,0]
-    nearestNeighborBlockIndex(&pLat, &pLon, nSou,
-                              &tarLat[0,0], &tarLon[0,0], &tarNNSouID[0],
-                              &tarNNDis[0,0], nTar, maxR)
+
+    cdef double * pLat = &psouLat[0, 0]
+    cdef double * pLon = &psouLon[0, 0]
+    nearestNeighborBlockIndex(& pLat, & pLon, nSou,
+                               & tarLat[0, 0], & tarLon[0, 0], & tarNNSouID[0],
+                               & tarNNDis[0, 0], nTar, maxR)
     return None
 
 
@@ -91,8 +86,9 @@ def interpolate_nn(np.ndarray[double, ndim=2, mode="c"] souVal not None,
                    np.ndarray[double, ndim=2, mode="c"] tarVal not None,
                    np.ndarray[int, ndim=1, mode="c"] tarNNSouID not None,
                    int nTar):
-    nnInterpolate(&souVal[0,0], &tarVal[0,0], &tarNNSouID[0], nTar)
+    nnInterpolate( & souVal[0, 0], & tarVal[0, 0], & tarNNSouID[0], nTar)
     return None
+
 
 def interpolate_summary(np.ndarray[double, ndim=2, mode="c"] souVal not None,
                         np.ndarray[int, ndim=1, mode="c"] souNNSouID not None,
@@ -101,13 +97,15 @@ def interpolate_summary(np.ndarray[double, ndim=2, mode="c"] souVal not None,
                         np.ndarray[double, ndim=2, mode="c"] tarSD not None,
                         np.ndarray[int, ndim=2, mode="c"] nSouPixels not None,
                         int nTar):
-    summaryInterpolate(&souVal[0,0], &souNNSouID[0], nSou,
-                       &tarVal[0,0], &tarSD[0,0], &nSouPixels[0,0], nTar)
+    summaryInterpolate( & souVal[0, 0], & souNNSouID[0], nSou,
+                       & tarVal[0, 0], & tarSD[0, 0], & nSouPixels[0, 0], nTar)
     return None
 
 # Check dimensions if src/trg lat/lon dimensions are 1 or 2.
+
+
 def check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
-    # Return error if dimension size is not 2.
+
     if psouLat.ndim < 1 or psouLat.ndim > 2:
         print('No. of source latitude dimensions should be 1 or 2.')
         return False
@@ -123,49 +121,51 @@ def check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
     if ptarLat.ndim != ptarLon.ndim:
         print('Target lat/lon dimension sizes do not match.')
         print('Lat dim = '+str(ptarLat.ndim))
-        print('Lon dim = '+str(ptarLon.ndim))        
+        print('Lon dim = '+str(ptarLon.ndim))
         return False
     if psouLat.ndim != psouLon.ndim:
         print('Source lat/lon dimension sizes do not match.')
         print('Lat dim = '+str(psouLat.ndim))
-        print('Lon dim = '+str(psouLon.ndim))        
-        return False    
+        print('Lon dim = '+str(psouLon.ndim))
+        return False
     return True
 
 # Wrapper for any projection using nn interpolation.
+
+
 def resample_n(psouLat, psouLon, ptarLat, ptarLon, psouVal, r):
     if check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
-        # Default - all 2d
+        # Default - all 2D
         slon = psouLon
         slat = psouLat
         tlon = ptarLon
         tlat = ptarLat
         sval = psouVal
-        
-        # If source is 1d lat/lon, reshape source value.
+
         if psouLat.ndim == 1:
-            # Generate 2d lat/lon.
-            print("generating 2d source lat/lon/src values...")            
+            # Generate 2D lat/lon.
             slat = psouLat.reshape(psouLat.size, 1)
             slon = psouLon.reshape(psouLon.size, 1)
+            # If source is 1D lat/lon, make source value 2D.
             sval = psouVal.reshape(psouVal.size, 1)
-        # If target is 1d lat/lon, return value is also 1d.            
         if ptarLat.ndim == 1:
-            print("generating 2d target lat/lon values...")            
             tlat = ptarLat.reshape(ptarLat.size, 1)
             tlon = ptarLon.reshape(ptarLon.size, 1)
             trg = resample(slat, slon, tlat, tlon, sval, r)
+            # If target is 1D lat/lon, return value should be 1D.
             return trg.ravel()
         else:
             return resample(slat, slon, tlat, tlon, sval, r)
     else:
         return None
-    
+
 # Wrapper for geographic projection using nn interpolation.
+
+
 def resample_n_g(psouLat, psouLon, ptarLat, ptarLon, psouVal, r):
     if check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
         if ptarLat.ndim == 1:
-            # Generate 2d lat/lon.
+            # Generate 2D lat/lon.
             lat, lon = np.meshgrid(ptarLon, ptarLat)
             latd = np.array(lat, dtype='float64')
             lond = np.array(lon, dtype='float64')
@@ -176,12 +176,14 @@ def resample_n_g(psouLat, psouLon, ptarLat, ptarLon, psouVal, r):
             return resample(psouLat, psouLon, ptarLat, ptarLon, psouVal, r)
     else:
         return None
-    
+
 # Wrapper for any projection using summary interpolation.
+
+
 def resample_s(psouLat, psouLon, ptarLat, ptarLon, psouVal, r,
                tarSD, nSouPixels):
     if check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
-        # Default - all 2d
+        # Default - all 2D
         slon = psouLon
         slat = psouLat
         tlon = ptarLon
@@ -189,37 +191,34 @@ def resample_s(psouLat, psouLon, ptarLat, ptarLon, psouVal, r,
         sval = psouVal
 
         if psouLat.ndim == 1:
-            # Generate 2d lat/lon.
-            print("generating 2d source lat/lon/src values...")
+            # Generate 2D lat/lon.
             slat = psouLat.reshape(psouLat.size, 1)
             slon = psouLon.reshape(psouLon.size, 1)
             sval = psouLon.reshape(psouVal.size, 1)
-        # If target is 1d lat/lon, return value is also 1d.            
         if ptarLat.ndim == 1:
-            print("generating 2d target lat/lon values...")            
             tlat = ptarLat.reshape(ptarLat.size, 1)
             tlon = ptarLon.reshape(ptarLon.size, 1)
             trg = resample(slat, slon, tlat, tlon, sval, r,
-                            True, tarSD, nSouPixels)
+                           True, tarSD, nSouPixels)
+            # If target is 1D lat/lon, return value should be 1D.
             return trg.ravel()
         else:
             return resample(slat, slon, tlat, tlon, sval, r,
                             True, tarSD, nSouPixels)
     else:
         return None
-    
+
 # Wrapper for geographic projection using summary interpolation.
+
+
 def resample_s_g(psouLat, psouLon, ptarLat, ptarLon, psouVal, r,
                  tarSD, nSouPixels):
     if check_dimensions(psouLat, psouLon, ptarLat, ptarLon, psouVal):
         if ptarLat.ndim == 1:
-            # Generate 2d lat/lon.
+            # Generate 2D lat/lon.
             lat, lon = np.meshgrid(ptarLat, ptarLon)
             latd = np.array(lat, dtype='float64')
             lond = np.array(lon, dtype='float64')
-            # latd = lat_.copy()
-            # lond = lon_.copy()
-            print("generating 2d lat/lon...")
             return resample(psouLat, psouLon,
                             latd, lond,
                             psouVal, r, True, tarSD, nSouPixels)
@@ -230,56 +229,42 @@ def resample_s_g(psouLat, psouLon, ptarLat, ptarLon, psouVal, r,
         return None
 
 # Wrapper for getting the target values.
+
+
 def resample(psouLat, psouLon, ptarLat, ptarLon, psouVal,
              r, s=False, tarSD=None, nSouPixels=None):
-    # Get the shape of lat/lon [2].
+
     nx = ptarLat.shape[1]
-    # print(nx)
     ny = ptarLat.shape[0]
-    # print(ny)
-    n_trg = nx * ny;
-    # print(n_trg)
+    n_trg = nx * ny
+
     if s is True:
-        print('using summary interpolation')
         if tarSD is None:
             print('Target std. dev. input is None.')
             return None
         if nSouPixels is None:
             print('Source no. pixel input is None')
             return None
-        
-        trg_data = np.zeros((ny,nx), dtype=psouVal.dtype)        
-        n_src = psouLat.size;
-        print(n_src)
+
+        trg_data = np.zeros((ny, nx), dtype=psouVal.dtype)
+        n_src = psouLat.size
         sx = psouLat.shape[0]
-        print(sx)
         sy = psouLat.shape[1]
-        print(sy)
         i = np.arange(n_src, dtype=np.int32)
-        d = np.arange(n_src, dtype=np.float64).reshape((sy,sx))
-        # tlat = ptarLat.copy()
-        # tlon = ptarLon.copy()
-        # slat = psouLat.copy()
-        # slon = psouLon.copy()
+        d = np.arange(n_src, dtype=np.float64).reshape((sy, sx))
         find_nn_block_index(ptarLat, ptarLon,
-#        find_nn_block_index(tlat, tlon,
                             n_trg,
                             psouLat, psouLon,
-#                            slat, slon,
                             i, d,
                             n_src,
                             r)
-        print('finished generating index.')
-        # print(index)
         interpolate_summary(psouVal, i, n_src,
                             trg_data, tarSD, nSouPixels, n_trg)
-        print('finished retrieving data with index.')
         return trg_data
     else:
-        print('using nn interpolation')
-        trg_data = np.zeros((ny,nx), dtype=psouVal.dtype)        
+        trg_data = np.zeros((ny, nx), dtype=psouVal.dtype)
         i = np.arange(nx*ny, dtype=np.int32)
-        d = np.arange(nx*ny, dtype=np.float64).reshape((ny,nx))        
+        d = np.arange(nx*ny, dtype=np.float64).reshape((ny, nx))
         find_nn_block_index(psouLat, psouLon,
                             psouLat.size,
                             ptarLat, ptarLon,
@@ -288,8 +273,3 @@ def resample(psouLat, psouLon, ptarLat, ptarLon, psouVal,
                             r)
         interpolate_nn(psouVal, trg_data, i, n_trg)
         return trg_data
-# References
-#
-# [1] https://stackoverflow.com/questions/40413858/how-to-handle-double-pointer-in-c-wrapping-by-cython
-# [2] https://github.com/cython/cython/wiki/tutorials-NumpyPointerToC
-# [3] https://stackoverflow.com/questions/51546710/pass-data-from-a-c-function-via-double-pointer-in-cython
