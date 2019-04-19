95384f718ceae32589540e45918de23e8aefb563
H. Joe Lee <hyoklee@hdfgroup.org>
2019-04-18 23:02:00 -0500

    UIACCESS-77:added the first unit test.

--- tests/test_resample_n.py
+++ tests/test_resample_n.py
@@ -1,8 +1,29 @@
 from unittest import TestCase
 
 import pytaf
+import numpy as np
+
 
 class TestResampleN(TestCase):
-    def test_is_string(self):
-        g = pytaf.resample_n()
-        self.assertTrue(g)
+    
+    def test_resample_n_1d(self):
+        slat = np.arange(12, dtype=np.float64)
+        slon = np.arange(12, dtype=np.float64)
+        sdata = slat * -333
+        tlat = np.arange(12, dtype=np.float64)
+        tlon = np.arange(12, dtype=np.float64)
+        # radius
+        r = 5555
+        g = pytaf.resample_n(slat, slon, tlat, tlon, sdata, r)
+        h = np.array([-0.,  -333.,  -666.,  -999., 
+                      -1332., -1665., -1998.,-2331.,
+                      -999., -2997.,  -3330., -3663.])
+        try:
+            np.testing.assert_array_equal(g, h)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)
+        # Use the following if you don't want to use numpy.testing.
+        # self.assertEqual(g.tolist(), h.tolist())
