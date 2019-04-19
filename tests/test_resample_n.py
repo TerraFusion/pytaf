4281ac418ebf26790ecfb3452fd51917e3032eaf
H. Joe Lee <hyoklee@hdfgroup.org>
2019-04-18 23:11:21 -0500

    UIACCESS-77:added 2d test for resample_n.

--- tests/test_resample_n.py
+++ tests/test_resample_n.py
@@ -12,7 +12,6 @@ class TestResampleN(TestCase):
         sdata = slat * -333
         tlat = np.arange(12, dtype=np.float64)
         tlon = np.arange(12, dtype=np.float64)
-        # radius
         r = 5555
         g = pytaf.resample_n(slat, slon, tlat, tlon, sdata, r)
         h = np.array([-0.,  -333.,  -666.,  -999., 
@@ -25,5 +24,22 @@ class TestResampleN(TestCase):
             res = False
             print(err)
         self.assertTrue(res)
-        # Use the following if you don't want to use numpy.testing.
-        # self.assertEqual(g.tolist(), h.tolist())
+
+    def test_resample_n_2d(self):
+        slat = np.arange(12, dtype=np.float64).reshape((3,4))
+        slon = np.arange(12, dtype=np.float64).reshape((3,4))
+        sdata = slat * -333
+        tlat = np.arange(12, dtype=np.float64).reshape((3,4))
+        tlon = np.arange(12, dtype=np.float64).reshape((3,4))
+        r = 5555
+        g = pytaf.resample_n(slat, slon, tlat, tlon, sdata, r)
+        h = np.array([[-0.,  -333.,  -666.,  -999.], 
+                      [-1332., -1665., -1998.,-2331.],
+                      [-999, -2997.,  -3330., -3663.]])
+        try:
+            np.testing.assert_array_equal(g, h)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)        
