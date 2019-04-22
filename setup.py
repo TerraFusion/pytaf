e2d8994a6cbb13b1bf91f6968c2b6ae1fb1d761a
H. Joe Lee <hyoklee@hdfgroup.org>
2019-04-22 13:01:45 -0500

    UIACCESS-82:added comment for setup.py. added project URL.

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+"""
+setup.py
+
+This script helps you test, install, and package pytaf module in a standard
+Python distribution manner.
+
+Usage:
+
+For installation, 
+
+ $python setup.py install
+
+For testing,
+
+ $python setup.py test
+
+Tested under: Python 3.6.6 :: Anaconda custom (64-bit)
+Last updated: 2019-04-22
+"""
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
@@ -10,6 +29,7 @@ setup(name='pytaf',
       description='Python wrapper for advanced funsion reprojection',
       author='TerraFusion Team',
       author_email='eoshelp@hdfgroup.org',
+      url = 'https://github.com/TerraFusion',
       test_suite='nose.collector',
       tests_require=['nose'],      
       cmdclass = {'build_ext':build_ext},
