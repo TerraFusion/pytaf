1cf9a33a54d404c048149f7cf7517dc2f803dd3d
H. Joe Lee <hyoklee@hdfgroup.org>
2019-04-22 20:34:46 -0500

    UIACCESS-82:cleaned up run.sh script.

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
+# Tihs script is useful for Cython interface development and testing without
+# installation. For example, you can run this scaript after you edit pytaf.pyx.
+#
+# Last Update: 2019/04/22
+
 # Clean up.
+python setup.py clean --all
 rm *.o
 rm *.so
 rm pytaf.c
 
-# Cython
+# Build Cython interface on the current working directory.
 python setup.py build_ext --inplace
-# python example.py
-python test_n.py
+
+# Test example scripts.
 python modis2ug.rn.py
 python modis2ug.rn.h5.py
-python test_s.py
 python modis2ug.rs.py
 python modis2ug.rs.h5.py
+python misr2ceres.py
+python misr2ceres.h5.py
