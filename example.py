c52f92cca5fadaad42a03bf8699312bd81058d41
Muqun Yang <myang6@hdfgroup.org>
2019-05-03 20:56:53 -0500

    UIACCESS-97, make the value easy to guess in the example.py. The original example.py changes to example2.py.

--- example.py
+++ example.py
@@ -11,75 +11,86 @@ print(b)
 pytaf.clip(a, b, 4)
 print('a value after clipping')
 print(a)
-print('b value before low-level nearest neighbor function')
-print(b)
-print('Test low-level nearest neighbor with fake ID and numbers of cell')
-pytaf.interpolate_nn(a, b, c, 3)
-print('b value after low-level nearest neighbor function')
-print(b)
-d = np.arange(12, dtype=np.float64).reshape((3,4))
-e = np.arange(12, dtype=np.float64).reshape((3,4))
-c1 = np.arange(12, dtype=np.int32).reshape((3,4))
-c2 =  np.arange(12, dtype=np.int32)
-print('Source value(a) before low-level summary interpolation')
-print(a)
-pytaf.interpolate_summary(a, c2, a.size, d, e, c1, d.size)
-print('Source value after low-level summary interpolation')
-print(d)
-print('Source SD value after low-level summary interpolation')
-print(e)
-print('Source pixel count value after low-level summary interpolation')
-print(c1)
-f = np.arange(12, dtype=np.float64).reshape((3,4))
-print('Testing block index build')
-print('Index ID value before testing')
-pytaf.find_nn_block_index(d, e, 3, a, b, c, f, 3, 10000.0)
-print('Index ID value after testing')
-print(c)
 
-# radius
-r = 5555
+print('Test high-level nearest neighbor resample(1D lat/lon).')
 
-print('Test high-level nearest neighbor resample.')
+# radius 150 km
+r = 150000.
 
-print('Source value(e) before testing')
+a= np.array([30,33,36,39],dtype=np.float64)
+b= np.array([40,43,46,49],dtype=np.float64)
+d= np.array([40,37,34,31],dtype=np.float64)
+e= np.array([50,47,44,41],dtype=np.float64)
+f=np.array([1,2,3,4],dtype=np.float64)
+print('Source value(f) before testing')
+#should be [1 2 3 4]
+print(f)
 g = pytaf.resample_n(a, b, d, e, f, r)
 print('Source value after resampling')
+#should be [4 3 2 1]
 print(g)
 
-sd = np.arange(12, dtype=np.float64).reshape((3,4))
-pc = np.arange(12, dtype=np.int32).reshape((3,4))
-
-print('Test summary resample.')
+print('Test high-level nearest neighbor resample(2D lat/lon).')
+a= np.array([30,33,36,39],dtype=np.float64).reshape((2,2))
+b= np.array([40,43,46,49],dtype=np.float64).reshape((2,2))
+d= np.array([40,37,34,31],dtype=np.float64).reshape((2,2))
+e= np.array([50,47,44,41],dtype=np.float64).reshape((2,2))
+f=np.array([1,2,3,4],dtype=np.float64).reshape((2,2))
 print('Source value(f) before testing')
+#should be [1 2] [3 4]
 print(f)
-g1 = pytaf.resample_s(a, b, d, e, f, r,sd,pc)
+g = pytaf.resample_n(a, b, d, e, f, r)
+print('Source value after resampling')
+#should be [4 3] [2 1]
+print(g)
+
+# radius 100 km
+r = 100000.
+
+print('Test summary resample(1D lat/lon).')
+a1 = np.array([30.5,31.5,32.5,33.5,36.5,37.5,38.5,39.5],dtype=np.float64)
+b1 = np.array([40.5,41.5,42.5,43.5,46.5,47.5,48.5,49.5],dtype=np.float64)
+d1 = np.array([39,37,33,31],dtype=np.float64)
+e1 = np.array([49,47,43,41],dtype=np.float64)
+f=np.array([1,3,5,7,9,11,13,15],dtype=np.float64)
+sd = np.arange(4, dtype=np.float64)
+pc = np.arange(4, dtype=np.int32)
+
+print('Source value(f) before testing')
+#should be [1 3 5 7 9 11 13 15]
+print(f)
+
+g1 = pytaf.resample_s(a1, b1, d1, e1, f, r,sd,pc)
 print('Source value(f) after testing')
+#should be [14 10 6 2]
 print(g1)
-print('Source SD value after  summary interpolation')
+print('Source SD value after summary interpolation')
+#should be [1 1 1 1]
 print(sd)
 print('Source pixel count value after  summary interpolation')
+#should be [2 2 2 2]
 print(pc)
 
-# Test 1D lat/lon in target. 
-k = np.arange(12,dtype=np.float64)
-l = np.arange(12,dtype=np.float64)
-
-# Test 1D var for target lat and 2D var for target lon.
-#print('Test dimension mismatch.')
-#m0 = pytaf.resample_n(a, b, k, e, f, r)
-
-# Test 1D lat/lon.
-print('Test 1D lat/lon nn.')
+print('Test summary resample(2D lat/lon).')
+a1 = np.array([30.5,31.5,32.5,33.5,36.5,37.5,38.5,39.5],dtype=np.float64).reshape(4,2)
+b1 = np.array([40.5,41.5,42.5,43.5,46.5,47.5,48.5,49.5],dtype=np.float64).reshape(4,2)
+d1 = np.array([39,37,33,31],dtype=np.float64).reshape(2,2)
+e1 = np.array([49,47,43,41],dtype=np.float64).reshape(2,2)
+f=np.array([1,3,5,7,9,11,13,15],dtype=np.float64).reshape(4,2)
+sd = np.arange(4, dtype=np.float64).reshape(2,2)
+pc = np.arange(4, dtype=np.int32).reshape(2,2)
 print('Source value(f) before testing')
+#should be [ 1 3] [5 7] [9 11] [13 15]
 print(f)
-m = pytaf.resample_n(a, b, k, l, f, r)
-print('Source value(m) after testing')
-print(m)
-print('Test 1D lat/lon summary.')
-print('Source value(f) before testing')
-print(f)
-n = pytaf.resample_s(a, b, k, l, f, r,sd,pc)
-print('Source value(n) after testing')
-print(n)
-#test 1-D resmaple_g geographic projection later
+
+g1 = pytaf.resample_s(a1, b1, d1, e1, f, r,sd,pc)
+print('Source value(f) after testing')
+#should be [14 10] [6 2]
+print(g1)
+print('Source SD value after summary interpolation')
+#should be [1 1] [1 1]
+print(sd)
+print('Source pixel count value after summary interpolation')
+#should be [2 2] [2 2]
+print(pc)
+
